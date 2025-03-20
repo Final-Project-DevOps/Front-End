6319468f2f658683505fffa84f7dab94bed1f1ca
syifaniads <syfaads16@gmail.com>
2025-03-20 22:13:06 +0700

    Add Navbar component to application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <p>
+          React Firebase DevOps Assignment
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
